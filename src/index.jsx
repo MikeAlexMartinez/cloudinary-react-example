9af3c015ccdba24457ea79571c66c3de43151b77
MikeAlexMartinez <mike.alex.martinez@gmail.com>
2017-09-14 19:35:29 +0100

    Add initial commit

---
+++ src/index.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { CloudinaryContext, Transformation, Image } from 'cloudinary-react';
+import { render } from 'react-dom';
+import axios from 'axios';
+
+class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      gallery: [],
+    };
+  }
+
+  componentDidMount(){
+    axios.get('http://res.cloudinary.com/di6bv5utg/image/list/test.json')
+      .then((res) => {
+        console.log(res.data.resources);
+        this.setState({ gallery: res.data.resources });
+      });
+  }
+
+  render() {
+      return (
+          <div className="main">
+              <h1>Galleria</h1>
+              <div className="gallery">
+                <CloudinaryContext cloudName="di6bv5utg">
+                  {
+                    this.state.gallery.map(data => {
+                      return (
+                        <div className="responsive" key={data.public_id}  >
+                          <div className="img">
+                            <a target="_blank" href={`http://res.cloudinary.com/di6bv5utg/image/upload/v1505145075/${data.public_id}.jpg`}>
+                              <Image publicId={data.public_id}></Image>
+                            </a>
+                            <div className="desc">Created at {data.created_at}</div>
+                          </div>
+                        </div>
+                      )
+                    })
+                  }
+                </CloudinaryContext>
+                <div className="clearfix"></div>
+              </div>
+          </div>
+      );
+  }
+}
+
+render(<Home />, document.getElementById('container'));
